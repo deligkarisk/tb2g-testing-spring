363fcb2009a4c6f5613123b5e394c3aeb30737e2
deligkarisk <deligkarisk@gmail.com>
2021-09-17 10:11:06 +0900

    Test properties tests

---
+++ src/test/java/org/springframework/samples/petclinic/kosmas/PropertiesTest.java
@@ -0,0 +1,32 @@
+package org.springframework.samples.petclinic.kosmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles("externalized")
+@TestPropertySource("classpath:yanny.properties")
+@SpringJUnitConfig(classes = PropertiesTest.TestConfig.class)
+public class PropertiesTest {
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.kosmas")
+    static class TestConfig {
+    }
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIheard() {
+        String word = hearingInterpreter.whatIheard();
+        assertEquals("YaNNy!", word);
+    }
+}
