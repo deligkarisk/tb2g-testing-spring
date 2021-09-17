b496c8e6e77ed5af99b960e02d6d32b04f8dae9f
deligkarisk <deligkarisk@gmail.com>
2021-09-17 09:58:46 +0900

    Active profiles refactoring

--- src/test/java/org/springframework/samples/petclinic/kosmas/HearingInterpreterActiveProfilesTest.java
+++ src/test/java/org/springframework/samples/petclinic/kosmas/HearingInterpreterActiveProfilesTest.java
@@ -2,23 +2,31 @@ package org.springframework.samples.petclinic.kosmas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ActiveProfiles("base-test")
-@SpringJUnitConfig(classes = {BaseConfig.class, YannyConfig.class})
-class HearingInterpreterYannyJUnit5Test {
+@SpringJUnitConfig(classes = HearingInterpreterActiveProfilesTest.TestConfig.class)
+@ActiveProfiles("yanny")
+class HearingInterpreterActiveProfilesTest {
+
+    @Configuration
+    @Profile("yanny")
+    @ComponentScan("org.springframework.samples.petclinic.kosmas")
+    static class TestConfig {
+    }
 
     @Autowired
     HearingInterpreter hearingInterpreter;
 
     @Test
     void whatIheard() {
-
         String word = hearingInterpreter.whatIheard();
         assertEquals("Yanny", word);
-
     }
+
 }
--- src/test/java/org/springframework/samples/petclinic/kosmas/HearingInterpreterPackageScanTest.java
+++ src/test/java/org/springframework/samples/petclinic/kosmas/HearingInterpreterPackageScanTest.java
@@ -4,13 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringJUnitConfig(classes = HearingInterpreterPackageScanTest.TestConfig.class)
+@ActiveProfiles("component-scan")
 class HearingInterpreterPackageScanTest {
 
+    @Profile("component-scan")
     @Configuration
     @ComponentScan("org.springframework.samples.petclinic.kosmas")
     static class TestConfig {
