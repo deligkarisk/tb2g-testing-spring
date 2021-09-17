e855401f6b3e5dfb3f3985a6e6fe562ffb6976be
deligkarisk <deligkarisk@gmail.com>
2021-09-17 09:23:26 +0900

    Configuration with inner classes

---
+++ src/test/java/org/springframework/samples/petclinic/kosmas/HearingInterpreterInnerClassTest.java
@@ -0,0 +1,33 @@
+package org.springframework.samples.petclinic.kosmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = HearingInterpreterInnerClassTest.TestConfig.class)
+class HearingInterpreterInnerClassTest {
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        HearingInterpreter hearingInterpreter() {
+            return new HearingInterpreter(new LaurelWordProducer());
+        }
+
+    }
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIheard() {
+
+        String word = hearingInterpreter.whatIheard();
+        assertEquals("Laurel", word);
+    }
+}
