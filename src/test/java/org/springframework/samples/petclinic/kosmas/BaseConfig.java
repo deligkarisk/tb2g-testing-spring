5810d82cec1c3b78215d63001c582dab492fefa8
deligkarisk <deligkarisk@gmail.com>
2021-09-17 09:00:01 +0900

    Spring tests with JUnit4

---
+++ src/test/java/org/springframework/samples/petclinic/kosmas/BaseConfig.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic.kosmas;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BaseConfig {
+
+    @Bean
+    HearingInterpreter hearingInterpreter(WordProducer wordProducer) {
+        return new HearingInterpreter(wordProducer);
+    }
+
+
+
+}
